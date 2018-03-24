2c43020098a74b2b2557d576356bdbf71301ad5e
Martin David Valentino <martin.david.siagian@gmail.com>
2018-03-25 08:33:02 +1100

    new css design

--- src/components/Dashboard/ActionChecklist.js
+++ src/components/Dashboard/ActionChecklist.js
@@ -3,22 +3,39 @@ import Box from 'grommet/components/Box';
 import CheckBox from 'grommet/components/CheckBox';
 import CustomCard from '../../components/Layout/Card';
 
+import './checkbox.css';
+
 const ActionChecklist = () => (
   <CustomCard>
     <h3>Your Action This Week</h3>
     <Box pad={{ between: 'medium' }}>
-      <CheckBox
-        label="Action 1"
-        toggle={false}
-      />
-      <CheckBox
-        label="Action 2"
-        toggle={false}
-      />
-      <CheckBox
-        label="Action 3"
-        toggle={false}
-      />
+      <ul className="list">
+        <li className="list-item">
+          <input type="checkbox" className="hidden-box" id="first" />
+          <label htmlFor="first" className="check--label">
+            <span className="check--label-box" />
+            <span className="check--label-text">Action 1</span>
+          </label>
+        </li>
+      </ul>
+      <ul className="list">
+        <li className="list-item">
+          <input type="checkbox" className="hidden-box" id="second" />
+          <label htmlFor="second" className="check--label">
+            <span className="check--label-box" />
+            <span className="check--label-text">Action 2</span>
+          </label>
+        </li>
+      </ul>
+      <ul className="list">
+        <li className="list-item">
+          <input type="checkbox" className="hidden-box" id="third" />
+          <label htmlFor="third" className="check--label">
+            <span className="check--label-box" />
+            <span className="check--label-text">Action 3</span>
+          </label>
+        </li>
+      </ul>
     </Box>
   </CustomCard>
 );
