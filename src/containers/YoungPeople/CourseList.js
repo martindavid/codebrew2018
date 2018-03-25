37408f6e51e17157af24f4e6fbde0049d98d0f83
William Kurniawan <kurniawanw0194@gmail.com>
2018-03-25 15:00:30 +1100

    Remove redundant header

--- src/containers/YoungPeople/CourseList.js
+++ src/containers/YoungPeople/CourseList.js
@@ -1,57 +1,36 @@
-import React, { Component } from 'react';
-import CourseCard from '../../components/Dashboard/CourseCard';
+// @flow
+import React from 'react';
 import Box from 'grommet/components/Box';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import CourseCard from '../../components/Dashboard/CourseCard';
+
+type Props = {
+  history: Object,
+};
 
 const StyledContainer = styled.div`
- padding: 16px;
+  padding: 16px;
   text-align: center;
   border-radius: 10px;
   background-color: white;
 `;
 
-const CourseList = props => (
+const CourseList = (props: Props) => (
   <StyledContainer>
-    <h2>Courses</h2>
-    <hr />
     <Box onClick={() => props.history.push('/yp/course-list/1')}>
-      <CourseCard
-        value={40}
-        courseName="Biology 101"
-        backgroundColor="#00BCD4"
-        isTaken
-      />
+      <CourseCard value={40} courseName="Biology 101" backgroundColor="#00BCD4" isTaken />
     </Box>
     <Box onClick={() => props.history.push('/yp/course-list/2')}>
-      <CourseCard
-        value={100}
-        courseName="Robotic 101"
-        backgroundColor="#E6EE9C"
-        isTaken
-      />
+      <CourseCard value={100} courseName="Robotic 101" backgroundColor="#E6EE9C" isTaken />
     </Box>
     <Box onClick={() => props.history.push('/yp/course-list/3')}>
-      <CourseCard
-        courseName="Have fun with Python"
-        value={70}
-        backgroundColor="#C5E1A5"
-        isTaken
-      />
+      <CourseCard courseName="Have fun with Python" value={70} backgroundColor="#C5E1A5" isTaken />
     </Box>
     <Box onClick={() => props.history.push('/yp/course-list/4')}>
-      <CourseCard
-        courseName="Intro to Web Development"
-        value={70}
-        backgroundColor="#9DC9AC"
-      />
+      <CourseCard courseName="Intro to Web Development" value={70} backgroundColor="#9DC9AC" />
     </Box>
     <Box onClick={() => props.history.push('/yp/course-list/5')}>
-      <CourseCard
-        courseName="Coding from Scratch"
-        value={70}
-        backgroundColor="#F1EDD0"
-      />
+      <CourseCard courseName="Coding from Scratch" value={70} backgroundColor="#F1EDD0" />
     </Box>
   </StyledContainer>
 );
